1aa6ded0006be9c39ef29225191566fe4d0eb66c
Mark Pearyer <marcusp619@gmail.com>
2015-04-07 14:56:48 -0700

    Create story.js

---
+++ story.js
@@ -0,0 +1,8 @@
+var noun= prompt('What noun?');
+var adjective = prompt('What adjective?');
+var verb = prompt('What verb?');
+alert('Ready for your story');
+var sentence = 'The next word should be the noun ' + noun;
+var sentence += 'The second word should be the adjective ' + adjective;
+var sentence += 'The third and final word should be the verb ' + verb;
+document.write(sentence);
