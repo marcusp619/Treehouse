4bc7e95624eba320d47bdfbf910569758337cb65
MARK PEARYER <markpearyer@MARKs-MacBook-Air.local>
2015-12-14 04:47:38 -1000

    Planned out task

---
+++ Using jQuery Plugins/project-todoList/js/app.js
@@ -0,0 +1,44 @@
+// Problem: User interaction doesnt provide anything
+// Solution: Add interactivity for managing daily task
+
+// Add a new task
+var addTask = function () {
+  //When button is pressed...
+  //Create new list item with the text from #new-task:
+    //label
+    //input (text)
+    //button.edit
+    //button.Delete
+    //Each elements, needs modified and appended
+};
+
+// Edit an existing task
+var editTask = function () {
+  //When button is pressed...
+    //if class of the parent is .editMode
+      //Switch  from .editMode
+      //label text become the input's value
+    //else
+      //Switch to .editMode
+      //input value becomes the label's text
+
+      // Toggle .editMode on the parent
+};
+
+// Delete existing task
+var deleteTask = function () {
+  //When the Delete button is pressed...
+    //Remove the parnt list item from the ul
+};
+
+// Mark task as complete
+var taskCompleted = function () {
+  // When the checkbox is checked...
+    //Append the task item to the #completed-tasks
+};
+
+// Mark task as incomplete
+var taskIncomplete = function () {
+      //When the checkbox is unchecked...
+        // Append the task item to #uncompleted-tasks
+};      
